31bc121a243d2c37a2ce401dea4772022dcb32a3
Ana Ribeiro <61992675+AnaMRibeiro@users.noreply.github.com>
2021-06-26 12:24:13 +0100

    commit1

---
+++ CotoviaSite2/CotoviaSite2/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using CotoviaSite2.Models;
+
+namespace CotoviaSite2.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<CotoviaSite2.Models.Noticias> Noticias { get; set; }
+    }
+}
